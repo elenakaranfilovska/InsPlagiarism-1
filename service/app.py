e6cce6392c56a0f93e0687da38e0af75ebe3352b
dimitarmileski <dime.mileski@hotmail.com>
2020-03-01 16:26:16 +0100

    Working wersion

--- service/app.py
+++ service/app.py
@@ -3,6 +3,7 @@ from flask_restplus import Api, Resource, fields
 import scipy
 from absl import logging
 from sklearn.metrics.pairwise import cosine_similarity
+import logging
 
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -54,6 +55,7 @@ model = app.model('Prediction params',
 class MainClass(Resource):
 
 	#######################################################
+	#print(request.json)
 
 	def embed(self, input):
 		module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
@@ -90,38 +92,54 @@ class MainClass(Resource):
 		response.headers.add('Access-Control-Allow-Methods', "*")
 		return response
 
-	@app.expect(model)		
+	@app.expect(model)
 	def post(self):
 		try:
 
 			formData = request.json
-			data = [val for val in formData.values()]
+			text1 = formData.get('fileupload1')
+			text2 = formData.get('fileupload2')
+			messages = []
+			messages1 = []
+			linesText1 = text1.split('\n')
+			for line in linesText1:
+				messages.append(line.rstrip())
+
+			print(messages)
+			linesText2 = text2.split('\n')
+			for line in linesText2:
+				messages1.append(line.rstrip())
+			print(messages1)
+
+
+			#data = [val for val in formData.values()]
 			# prediction = classifier.predict(data)
 			data = []
-
+			print("DATA[]")
 
 			# @title Compute a representation for each message, showing various lengths supported.
-			word = "Plagiarisma"
-			sentence = "Best plagiarism detector for you."
-			paragraph = ("French to Dutch translation results for 'plagiat' designed for tablets and mobile devices."
-						 "Possible languages include English, Dutch, German, French, Spanish.")
-			messages = [word, sentence, paragraph]
+			# word = "Plagiarisma"
+			# sentence = "Best plagiarism detector for you."
+			# paragraph = ("French to Dutch translation results for 'plagiat' designed for tablets and mobile devices."
+			# 			 "Possible languages include English, Dutch, German, French, Spanish.")
+			# messages = [word, sentence, paragraph]
 
-			word1 = "Elephant"
-			sentence1 = "Best plagiarism detector for you."
-			paragraph1 = (
-				"English is a West Germanic language that was first spoken in early medieval England,"
-				" eventually became a global lingua franca."
-				"It is named after the Angles."
-			)
-			messages1 = [word1, sentence1, paragraph1]
+			# word1 = "Elephant"
+			# sentence1 = "Best plagiarism detector for you."
+			# paragraph1 = (
+			# 	"English is a West Germanic language that was first spoken in early medieval England,"
+			# 	" eventually became a global lingua franca."
+			# 	"It is named after the Angles."
+			# )
+			# messages1 = [word1, sentence1, paragraph1]
 
 			# Reduce logging output.
-			logging.set_verbosity(logging.ERROR)
-
+			#logging.set_verbosity(logging.ERROR)
+			print("Logging[]")
 			message_embeddings1 = self.embed(messages)
 			message_embeddings2 = self.embed(messages1)
 
+			print("message Embedings[]")
 			# Similarity matrix
 			similarity_matrix = cosine_similarity(message_embeddings1, message_embeddings2)
 			num_similar_sentences = 0
@@ -130,13 +148,15 @@ class MainClass(Resource):
 				for j in range(len(similarity_matrix[i])):
 					if similarity_matrix[i][j] > 0.90:
 						num_similar_sentences = num_similar_sentences + 1
-
+			print("Filtering []")
 			print(num_similar_sentences)
 			print(len(similarity_matrix))
 			if ((num_similar_sentences / len(similarity_matrix)) > 0.5):
 				data = ['PLAGIAT!']
 			else:
 				data = ['NOT PLAGIAT.']
+
+			print(data)
 
 			response = jsonify({
 				"statusCode": 200,
@@ -151,3 +171,5 @@ class MainClass(Resource):
 				"status": "Could not make prediction",
 				"error": str(error)
 			})
+
+
--- ui/src/App.js
+++ ui/src/App.js
@@ -15,6 +15,137 @@ class App extends Component {
     this.state = {
       isLoading: false,
       formData: {
+        fileupload1: '',
+        fileupload2: '',
+      },
+      result: ""
+    };
+  }
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    var formData = this.state.formData;
+    formData[name] = value;
+    this.setState({
+      formData
+    });
+  }
+  loadFileAsText = (event) => {
+  var file= event.target.files[0];
+  var name=event.target.name;
+    let reader=new FileReader()
+     var formData = this.state.formData;
+     var content="";
+  reader.onload = function() {
+    formData[name]=reader.result;
+  };
+    reader.readAsText(file);
+    this.setState({
+      formData
+    });
+  }
+
+  handlePredictClick = (event) => {
+    const formData = this.state.formData;
+    this.setState({ isLoading: true });
+    fetch('http://127.0.0.1:5000/prediction/',
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(formData)
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          result: response.result,
+          isLoading: false
+        });
+      });
+  }
+
+  handleCancelClick = (event) => {
+    this.setState({ result: "" });
+  }
+
+
+  render() {
+    const isLoading = this.state.isLoading;
+    const formData = this.state.formData;
+    const result = this.state.result;
+
+    return (
+      <Container>
+        <div>
+          <h1 className="title">ML React App</h1>
+        </div>
+        <div className="content">
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Label>File 1</Form.Label>
+                 <Form.Control
+                  type="file"
+                  placeholder="Insert File"
+                  name="fileupload1"
+                  id="fileToLoad"
+                  onChange={this.loadFileAsText} />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>File 2</Form.Label>
+                <Form.Control
+                  type="file"
+                  placeholder="Insert File"
+                  name="fileupload2"
+                  onChange={this.loadFileAsText} />
+              </Form.Group>
+            </Form.Row>
+            <Row>
+              <Col>
+                <Button
+                  block
+                  variant="success"
+                  disabled={isLoading}
+                  onClick={!isLoading ? this.handlePredictClick : null}>
+                  { isLoading ? 'Making prediction' : 'Predict' }
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+          {result === "" ? null :
+            (<Row>
+              <Col className="result-container">
+                <h5 id="result">{result}</h5>
+              </Col>
+            </Row>)
+          }
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default App;
+/*import React, { Component } from 'react';
+import './App.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      formData: {
         textfield1: '',
         textfield2: '',
         select1: 1,
@@ -168,3 +299,4 @@ class App extends Component {
 }
 
 export default App;
+*/
